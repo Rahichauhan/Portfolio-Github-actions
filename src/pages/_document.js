40dba51fee1deb3ea026a6f9e29998f52ccb1160
Rahichauhan <rahichauhan36@gmail.com>
2024-08-23 09:06:32 +0530

    Done

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,17 +3,24 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6DZG57JG5F"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-6DZG57JG5F');
+        `}} />
+      </Head>
       <body>
-        <script id="theme-switcher" strategy="beforeInteractive">
-          {
-            `if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-              document.documentElement.classList.add('dark')
-            } else {
-              document.documentElement.classList.remove('dark')
-            }`
+        <script id="theme-switcher" dangerouslySetInnerHTML={{ __html: `
+          if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+          } else {
+            document.documentElement.classList.remove('dark');
           }
-        </script>
+        `}} strategy="beforeInteractive" />
         <Main />
         <NextScript />
       </body>
